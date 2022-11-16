5c7ed73c8d2f9952980a1830126f69f9398e36a1
Oduola Kamol <you@example.com>
2022-11-16 11:52:14 +0100

    finally done

--- src/Places.js
+++ src/Places.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import './Places.css'
 import logo from './images/logo.png'
+import frame from './images/Frame 13.png'
+import butter from './images/002.png'
+import hut from './images/04.png'
+import coast from './images/06.png'
+import darkp from './images/08.png'
+import purple from './images/001.png'
+import cream from './images/03.png'
+import purplegreen from './images/001.png'
+import purplemosq from './images/001.png'
 
 const Places = () => {
   return (
@@ -33,7 +42,26 @@ const Places = () => {
             <li>Carbins</li>
             <li>Off grid</li>
             <li>Farm</li>
+            <li><img src={frame} alt='search'/></li>
         </ul>
+        <div className='homes'>
+        <img src={butter} alt='card one'/>
+            <img src={coast} alt='card two'/>
+            <img src={darkp} alt='card three'/>
+            <img src={hut} alt='card four'/>
+            <img src={purple} alt='card five'/>
+            <img src={cream} alt='card six'/>
+            <img src={purplegreen} alt='card seven'/>
+            <img src={purplemosq} alt='card eight'/>
+            <img src={butter} alt='card one'/>
+            <img src={coast} alt='card two'/>
+            <img src={darkp} alt='card three'/>
+            <img src={hut} alt='card four'/>
+            <img src={purple} alt='card five'/>
+            <img src={cream} alt='card six'/>
+            <img src={purplegreen} alt='card seven'/>
+            <img src={purplemosq} alt='card eight'/>
+        </div>
     </div>
   )
 }
